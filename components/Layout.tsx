128a68067e3eb610c283c046ef6e4f979dd83d7d
Musashi Sakamoto <1292602b@gmail.com>
2020-05-27 20:39:29 -0700

    レスポンシブメニュー

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { AppBar, Button, makeStyles, Toolbar, Typography, Box, Container, IconButton, Menu, MenuItem } from '@material-ui/core';
+import MoreIcon from '@material-ui/icons/MoreVert'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+}));
 
 type Props = {
   title?: string
@@ -9,35 +32,140 @@ type Props = {
 const Layout: React.FunctionComponent<Props> = ({
   children,
   title = 'This is the default title',
-}) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+}) => {
+  const classes = useStyles();
+
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <Button color="inherit">
+          <Link href="/about_me">
+            <a>
+              <Typography color="inherit">About me</Typography>
+            </a>
+          </Link>
+        </Button>
+      </MenuItem>
+      <MenuItem>
+        <Button color="inherit">
+          <Link href="/interests">
+            <a>
+              <Typography color="inherit">Interests</Typography>
+            </a>
+          </Link>
+        </Button>
+      </MenuItem>
+      <MenuItem>
+        <Button color="inherit">
+        <Link href="/portfolios">
+          <a>
+            <Typography color="inherit">Portfolios</Typography>
+          </a>
+        </Link>
+      </Button>
+      </MenuItem>
+      <MenuItem>
+        <Button color="inherit">
+          <Link href="/contact">
+            <a>
+              <Typography color="inherit">Contact</Typography>
+            </a>
+          </Link>
+        </Button>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <AppBar position="fixed">
+          <Toolbar variant="dense">
+            <Typography variant="h6" className={classes.title}>
+              Musashi Sakamoto
+            </Typography>
+
+            <div className={classes.sectionDesktop}>
+              <Button color="inherit">
+                <Link href="/about_me">
+                  <a style={{ color: 'white' }}>
+                    <Typography color="inherit">About me</Typography>
+                  </a>
+                </Link>
+              </Button>
+
+              <Button color="inherit">
+                <Link href="/interests">
+                  <a style={{ color: 'white' }}>
+                    <Typography color="inherit">Interests</Typography>
+                  </a>
+                </Link>
+              </Button>
+    
+              <Button color="inherit">
+                <Link href="/portfolios">
+                  <a style={{ color: 'white' }}>
+                    <Typography color="inherit">Portfolios</Typography>
+                  </a>
+                </Link>
+              </Button>
+    
+              <Button color="inherit">
+                <Link href="/contact">
+                  <a style={{ color: 'white' }}>
+                    <Typography color="inherit">Contact</Typography>
+                  </a>
+                </Link>
+              </Button>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label="show more"
+                aria-controls='primary-search-account-menu-mobile'
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+          </div>
+          </Toolbar>
+        </AppBar>
+        {renderMobileMenu}
+          <Box marginTop={8}>
+            {children}
+          </Box>
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  )
+}
 
 export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import { Grid, makeStyles } from '@material-ui/core'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const useStyles = makeStyles(theme => ({
+  section: {
+    height: '100vh',
+  },
+  greyBackground: {
+    backgroundColor: '#AAA'
+  }
+}));
+
+const IndexPage = () => {
+  const classes = useStyles();
+  return (
+    <Layout title="Musashi Sakamoto">
+      <Grid container className={classes.section}>
+        <h1>Hello Next.js 👋</h1>
+      </Grid>
+      <Grid container className={`${classes.section} ${classes.greyBackground}`}>
+        <h1>Hello Next.js2 👋</h1>
+      </Grid>
+      <Grid container className={classes.section}>
+        <h1>Hello Next.js3 👋</h1>
+      </Grid>
+      <Grid container className={`${classes.section} ${classes.greyBackground}`}>
+        <h1>Hello Next.js4 👋</h1>
+      </Grid>
+    </Layout>
+  )
+}
 
 export default IndexPage
