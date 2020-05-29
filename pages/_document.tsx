e85f007f484a64d8ce186b430f7e9988149d7779
Musashi Sakamoto <1292602b@gmail.com>
2020-05-29 16:05:55 -0700

    minor fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
