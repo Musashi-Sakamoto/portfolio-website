5112c3bf84c622ea601e0fd3d66cae86bef1c2b1
Musashi Sakamoto <1292602b@gmail.com>
2020-05-30 16:30:04 -0700

    画像リンクを変更

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -60,7 +60,7 @@ const Portfolio = () => {
               description="Simple 'Rock Scissors Paper' game you can do with your friend online. I came up with this idea when I was talking on Skype. My friend and I were about to decide something. Then I thought it'd be interesting if we had something like this. Created using React, Express.js, Socket.io, Heroku"
               pageLink='https://frosty-wozniak-9e9679.netlify.app/'
               githubLink='https://github.com/Musashi-Sakamoto/RockScissorsPaper'
-              imageUrl='https://i.gyazo.com/6bf2a19b916e7c12d9cda6fb446b468a.png' />
+              imageUrl='https://i.gyazo.com/a51bedfe584a27e3beac0540b6eb6127.png' />
           </Grid>
         </Grid>
       </Grid>
